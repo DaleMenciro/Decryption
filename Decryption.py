279a0eab72ca18c96f906532dd8cc686c38ce27b
Dale Menciro <dale_menciro@protonmail.com>
2023-04-17 12:59:52 +0800

    adding horizontal bar to make it aesthetically pleasing

--- Decryption.py
+++ Decryption.py
@@ -25,6 +25,7 @@ print(Style.DIM + Fore.LIGHTGREEN_EX + u"\u2500" * 50)
 encrypted_data= input(Style.BRIGHT + Fore.MAGENTA + "Kindly enter your encrypted data: ")
 print(Style.DIM + Fore.LIGHTGREEN_EX + u"\u2500" * 50, end= "\n ")
 print(Fore.GREEN + Style.NORMAL + "You entered:",encrypted_data)
+print(Style.DIM + Fore.LIGHTGREEN_EX + u"\u2500" * 50 + Style.RESET_ALL, end= "\n ")
 #Calling the def function to decrypt the user's input
 decrypted_data= decrypt_data(encrypted_data)
 
