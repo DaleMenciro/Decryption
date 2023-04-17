25f52c7c49a108a1eb8e6e4f2581dc515e2655b6
Dale Menciro <dale_menciro@protonmail.com>
2023-04-17 12:35:07 +0800

    Pseudocode

---
+++ Decryption.py
@@ -0,0 +1,13 @@
+#Code Starts
+
+#Def function for the conditions in decrypting
+    #if it has "*", change to "a"
+    #if it has "&", change to "e"
+    #if it has "#", change to "i"
+    #if it has "+", change to "o"
+    #if it has "!", change to "u"
+
+#Asks the user to input the encrypted data
+
+#Calling the def function to decrypt the user's input
+#Print the decrypted text
