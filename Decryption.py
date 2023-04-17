6a0f61dcaa184bdbc9b1033ae84736d358afe419
Dale Menciro <dale_menciro@protonmail.com>
2023-04-17 12:47:24 +0800

    Replace function to change "&" to "e" (With style and color)

--- Decryption.py
+++ Decryption.py
@@ -10,6 +10,7 @@ def decrypt_data(encrypted_data):
     #if it has "*", change to "a"
     decrypted_data= encrypted_data.replace("*",Fore.BLUE +"a"+ Style.RESET_ALL)
     #if it has "&", change to "e"
+    decrypted_data= decrypted_data.replace("&",Fore.GREEN +"e"+ Style.RESET_ALL)
     #if it has "#", change to "i"
     #if it has "+", change to "o"
     #if it has "!", change to "u"
