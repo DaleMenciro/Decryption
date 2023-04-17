36667460bcdfe33b5871fa7007a96c2aac6ed710
Dale Menciro <dale_menciro@protonmail.com>
2023-04-17 12:59:00 +0800

    printing the encrypted data that the user entered

--- Decryption.py
+++ Decryption.py
@@ -24,6 +24,7 @@ def decrypt_data(encrypted_data):
 print(Style.DIM + Fore.LIGHTGREEN_EX + u"\u2500" * 50)
 encrypted_data= input(Style.BRIGHT + Fore.MAGENTA + "Kindly enter your encrypted data: ")
 print(Style.DIM + Fore.LIGHTGREEN_EX + u"\u2500" * 50, end= "\n ")
+print(Fore.GREEN + Style.NORMAL + "You entered:",encrypted_data)
 #Calling the def function to decrypt the user's input
 decrypted_data= decrypt_data(encrypted_data)
 
