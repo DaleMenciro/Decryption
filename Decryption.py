c95cb45be0d9ba0a255a7b733d558991426cd396
Dale Menciro <dale_menciro@protonmail.com>
2023-04-17 12:46:09 +0800

    Replace function to change "*" to "a" (With style and color)

--- Decryption.py
+++ Decryption.py
@@ -8,6 +8,7 @@ from colorama import Fore,Style
 #Def function for the conditions in decrypting
 def decrypt_data(encrypted_data):
     #if it has "*", change to "a"
+    decrypted_data= encrypted_data.replace("*",Fore.BLUE +"a"+ Style.RESET_ALL)
     #if it has "&", change to "e"
     #if it has "#", change to "i"
     #if it has "+", change to "o"
